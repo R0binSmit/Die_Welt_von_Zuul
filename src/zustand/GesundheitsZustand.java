e715d69fac1c0292cfecda7fa78a0e708c7b5575
Nyxero <Nyxero@Nyxero-PC>
2017-08-30 18:43:39 +0200

    Neue Methode für eine Stustandsmanipulation: -kleineWiederbelebung -grosseWiederbelebung

--- src/zustand/GesundheitsZustand.java
+++ src/zustand/GesundheitsZustand.java
@@ -2,12 +2,10 @@ package zustand;
 
 public interface GesundheitsZustand {
 	GesundheitsZustand kleineHeilung();
-
 	GesundheitsZustand grosseHeilung();
-
 	GesundheitsZustand leichtVerletzen();
-
 	GesundheitsZustand schwerVerletzen();
-
 	GesundheitsZustand toeten();
+	GesundheitsZustand kleineWiederbelebung();
+	GesundheitsZustand grosseWiederbelebung();
 }
