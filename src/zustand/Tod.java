e60b0bf07ffc09be7352ad1b55776d9d807d6ce3
robin253 <robin253@w004-20.szut.uni-bremen.de>
2017-08-30 14:49:10 +0000

    Add Tod als neuen Zustand

---
+++ src/zustand/Tod.java
@@ -0,0 +1,32 @@
+package zustand;
+
+public class Tod implements GesundheitsZustand {
+	private static Tod instance = new Tod();
+	
+	private Tod() {	
+	}
+	
+	public static Tod getInstance() {
+		return instance;
+	}
+	
+	public GesundheitsZustand kleineHeilung() {
+		return instance;
+	}
+	
+	public GesundheitsZustand grosseHeilung() {
+		return instance;
+	}
+	
+	public GesundheitsZustand leichtVerletzen() {
+		return instance;
+	}
+	
+	public GesundheitsZustand schwerVerletzen() {
+		return instance;
+	}
+	
+	public GesundheitsZustand toeten(){
+		return instance;
+	}
+}
