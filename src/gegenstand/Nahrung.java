6a51c8c2b152a05f58acf4c592f64bfb370933a3
Frank <Frank@Frank-PC.fritz.box>
2017-08-30 19:38:08 +0200

    Leerzeichen

--- src/gegenstand/Nahrung.java
+++ src/gegenstand/Nahrung.java
@@ -1,6 +1,6 @@
 package gegenstand;
 
-public class Nahrung extends Gegenstand{
+public class Nahrung extends Gegenstand {
 	public Nahrung(String name, String beschreibung, int gewicht) {
 		super(name, beschreibung, gewicht);
 	}
