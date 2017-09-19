4be6ae38d274129e403dd028d0a0a5290ee238d1
Nyxero <Nyxero@web.de>
2017-09-19 20:28:09 +0200

    Delete Hose.java
    Gibt es nicht mehr.

--- src/item/Hose.java
+++
@@ -1,9 +0,0 @@
-package item;
-
-import javafx.scene.image.Image;
-
-public class Hose extends Verteidigung {
-	public Hose(String name, String beschreibung, int gewicht, int preis, Image image, int x, int y, double block) {
-		super(name, beschreibung, gewicht, preis, image, x, y, block);
-	}
-}
