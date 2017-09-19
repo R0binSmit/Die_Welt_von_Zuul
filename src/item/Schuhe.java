60c45c5f71302a2f45af532ca3afa7471dc30be1
Nyxero <Nyxero@web.de>
2017-09-19 20:28:30 +0200

    Delete Schuhe.java
    Gibt es nicht mehr.

--- src/item/Schuhe.java
+++
@@ -1,9 +0,0 @@
-package item;
-
-import javafx.scene.image.Image;
-
-public class Schuhe extends Verteidigung {
-	public Schuhe(String name, String beschreibung, int gewicht, int preis, Image image, int x, int y, double block) {
-		super(name, beschreibung, gewicht, preis, image, x, y, block);
-	}
-}
