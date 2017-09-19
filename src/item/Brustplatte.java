a7ebdb79b97e1869d6364f5d3df281cfe81e8b69
Nyxero <Nyxero@web.de>
2017-09-19 20:27:40 +0200

    Delete Brustplatte.java
    Gibt es nicht mehr.

--- src/item/Brustplatte.java
+++
@@ -1,9 +0,0 @@
-package item;
-
-import javafx.scene.image.Image;
-
-public class Brustplatte extends Verteidigung {
-	public Brustplatte(String name, String beschreibung, int gewicht, int preis, Image image, int x, int y, double block) {
-		super(name, beschreibung, gewicht, preis, image, x, y, block);
-	}
-}
