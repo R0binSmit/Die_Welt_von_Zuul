9e63c47b57770c32354fa80611809b2345306786
Nyxero <Nyxero@DESKTOP-SPURVN6.multi.box>
2017-09-21 19:29:23 +0200

    IEquipment Kommentiert.

--- src/equipment/IEquipment.java
+++ src/equipment/IEquipment.java
@@ -1,7 +1,11 @@
 package equipment;
 
+/**
+ * Dieses Inteface wird verwendet um die Klasse Defense und Weapon dazu zu zwingen,
+ * die unten genannten Methoden zu implementieren.
+ * 
+ */
 public interface IEquipment {
 	double getArmor();
-
 	int getDamage();
 }
