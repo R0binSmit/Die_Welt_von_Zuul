aed0acc62f4e56f13308ae1e3c74213898ba246b
Nyxero <Nyxero@web.de>
2017-09-21 12:07:26 +0000

    Update Defense.java
    items usable

--- src/item/Defense.java
+++ src/item/Defense.java
@@ -3,24 +3,33 @@ package item;
 import javafx.geometry.Point2D;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import item.EnumDefense;
+import character.Character;
 
-public abstract class Defense extends Item{
+public abstract class Defense extends Item {
 	private double armor;
-	
-	public Defense(String name, String description, int price, Image image, int x, int y, GraphicsContext graphicsContext, int armor) {
+	private EnumDefense defenseType;
+
+	public Defense(String name, String description, int price, EnumDefense defenseType, Image image, int x, int y,
+			GraphicsContext graphicsContext, int armor) {
 		super(name, description, price, image, new Point2D(x, y), graphicsContext);
 		this.armor = armor;
+		this.defenseType = defenseType;
 	}
-	
+
 	public void use(Character character) {
-		
+			character.equipItem(this);
 	}
-	
+
 	public double getArmor() {
 		return armor;
 	}
-	
+
 	public int getDamage() {
 		return 0;
+	}
+	
+	public EnumDefense getDefenseType() {
+		return defenseType;
 	}
 }
