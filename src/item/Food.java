20211b86bef95e6ee15187ce5ff9606fe8432081
Nyxero <Nyxero@web.de>
2017-09-21 12:10:46 +0000

    Update Food.java
    item usable

--- src/item/Food.java
+++ src/item/Food.java
@@ -3,13 +3,17 @@ package item;
 import javafx.geometry.Point2D;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import character.Character;
 
 public class Food extends Item{
-	public Food(String name, String description, int price, Image image, int x, int y, GraphicsContext graphicsContext) {
+	private int changeHealthPoints;
+	public Food(String name, String description, int price, Image image, int x, int y, GraphicsContext graphicsContext, int changeHealthPoints) {
 		super(name, description, price, image, new Point2D(x,y ), graphicsContext);
+		this.changeHealthPoints = changeHealthPoints;
 	}
 	
 	public void use(Character character) {
-		
+		int newHealthPoints = (character.getHP() + changeHealthPoints);
+		character.setHP(newHealthPoints);
 	}
 }
